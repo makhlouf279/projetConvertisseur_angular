29d7cc8818317e69cf2266b3f897d298c8e56089
Makhlouf <makhloufaroua@gmail.com>
2021-06-13 15:08:50 -0400

    mon projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
+import { Money } from './../money';
 import { Component } from '@angular/core';
+import { MoneyService } from './money.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'convertisseur';
+  moneyservice:MoneyService;
+  myMoney?:Money[];
+  montant?:any;
+  statut:boolean=true;
+  stat:boolean= true;
+  rate?:Money[];
+  resultat?:number;
+
+
+
+// injection de service
+  constructor(service:MoneyService){
+    this.moneyservice=service;
+  }
+
+  // appelle de la methode service
+  ngOnInit(){
+    this.moneyservice.getMoney().subscribe(
+      (data: Money[] | undefined) =>{
+        this.myMoney= data;
+      })
+
+  }
+
+  getLesMoney(rate:any){
+    this.rate=rate;
+
+
+  }
+
+  changerDirection(){
+    if(this.statut=!this.statut){
+        this.stat=false;
+    }else
+        this.stat=true;
+
+  }
+
+  convertirMoney(){
+      this.resultat=<any>this.montant*<any>this.rate;
+      return this.resultat;
+
+  }
+
 }
