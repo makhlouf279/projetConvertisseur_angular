6247fd674277640c54b239a89f433266c66ad4f4
mohsen <mohsen@tlati.ca>
2021-06-15 11:27:10 -0400

    fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Money } from './../money';
-import { Component } from '@angular/core';
-import { MoneyService } from './money.service';
-import { OnInit } from '@angular/core';
+import {Money} from './../money';
+import {Component} from '@angular/core';
+import {MoneyService} from './money.service';
+import {OnInit} from '@angular/core';
 import {map} from "rxjs/operators";
 
 
@@ -10,61 +10,50 @@ import {map} from "rxjs/operators";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'convertisseur';
-  moneyservice:MoneyService;
-  montant?:number=1;
-  statut:boolean=true;
-  stat:boolean= true;
-  resultat?:number=1;
-  selectCurency?: Money;
+  moneyService: MoneyService;
+  canadianDollarAmount?: string ;
+  CurrencyAmount?: string;
+  selectCurrency?: Money;
+  selectCurrencyIndex?: number;
+  currencies: Array<Money> = [];
 
-
-   map: Map<string, Money> = new Map<string, Money>();
-// injection de service
-  constructor(service:MoneyService){
-    this.moneyservice=service;
+  constructor(service: MoneyService) {
+    this.moneyService = service;
   }
 
-  // appelle de la methode service
-  ngOnInit(){
-    this.moneyservice.getMoney().subscribe(
-      (data: any | undefined) =>{
+  ngOnInit() {
+    this.moneyService.getMoney().subscribe(
+      (data: any | undefined) => {
         for (let code in data) {
-          this.map.set(data[code].name, data[code]);
+          this.currencies.push(data[code]);
         }
+        this.selectCurrency = this.currencies[0];
+        this.selectCurrencyIndex = 0;
       })
   }
 
-  changerDirection(){
-    if(this.statut=!this.statut){
-        this.stat=false;
-    }else
-        this.stat=true;
-
-  }
-
-  convertirMoney(){
-    if(this.montant!=undefined&& this.montant>=0 &&this.selectCurency?.rate!=undefined&& this.selectCurency?.rate>=0){
-      this.resultat=this.montant*this.selectCurency?.rate;
-    }else {
-      this.resultat=0;
+  converterMoney() {
+    if (this.canadianDollarAmount != undefined && parseFloat(this.canadianDollarAmount) >= 0 && this.selectCurrency?.rate != undefined && this.selectCurrency?.rate >= 0) {
+      this.CurrencyAmount = (parseFloat(this.canadianDollarAmount) * this.selectCurrency?.rate).toFixed(2);
+    } else {
+      this.CurrencyAmount = "0.00";
     }
-      return this.resultat;
-
+    return this.CurrencyAmount;
   }
 
-  convertirMoneyInverse(){
-    if(this.resultat!=undefined&& this.resultat>=0 &&this.selectCurency?.inverseRate!=undefined&& this.selectCurency?.inverseRate>=0){
-      this.montant=this.resultat*this.selectCurency?.inverseRate;
-    }else {
-      this.montant=0;
+  converterMoneyInverse() {
+    if (this.CurrencyAmount != undefined && parseFloat(this.CurrencyAmount) >= 0 && this.selectCurrency?.inverseRate != undefined && this.selectCurrency?.inverseRate >= 0) {
+      this.canadianDollarAmount = (parseFloat(this.CurrencyAmount) * this.selectCurrency?.inverseRate).toFixed(2);
+    } else {
+      this.canadianDollarAmount = "0.00";
     }
-      return this.montant;
-
+    return this.canadianDollarAmount;
   }
 
-  selectCurenecy(event: any) {
-   this.selectCurency = this.map.get(event.target.value);
+  selectCurrencyChange(event: any) {
+    this.selectCurrency = this.currencies.find(i => i.name == event.target.value);
+    this.selectCurrencyIndex = this.currencies.findIndex(i => i.name == event.target.value);
   }
 }
